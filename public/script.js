67871e4ca89d49553bfc9096efa9670f89a1d502
Macnelson9 <michaelofatu@gmail.com>
2025-06-18 20:58:04 +0100

    Enhance responsive design and improve navigation functionality

--- public/script.js
+++ public/script.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('Script is running!');
+const hamburger = document.querySelector('.fa-bars');
+const closeBtn = document.querySelector('.fa-close');
+const nav = document.getElementById('mobile-navigation');
+hamburger.addEventListener('click', function () {
+    nav.style.display = 'flex';
+    nav.style.opacity = '1';
+    nav.style.visibility = 'visible';
+});
+closeBtn.addEventListener('click', function () {
+    nav.style.display = 'none';
+    nav.style.opacity = '0';
+    nav.style.visibility = 'hidden';
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log('Script is running!');
+const hamburger = document.querySelector('.fa-bars') as HTMLElement;
+const closeBtn = document.querySelector('.fa-close') as HTMLElement;
+const nav = document.getElementById('mobile-navigation') as HTMLElement;
+
+hamburger.addEventListener('click', function () {
+  nav.style.display = 'flex';
+  nav.style.opacity = '1';
+  nav.style.visibility = 'visible';
+});
+
+closeBtn.addEventListener('click', function () {
+  nav.style.display = 'none';
+  nav.style.opacity = '0';
+  nav.style.visibility = 'hidden';
+});
